66ce6512da810ac465548a12868fcdce7f2e8524
Mohammed Maroof <19404@TQ.INV>
2024-10-16 11:15:13 +0400

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Employee;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,8 +17,42 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'a@a.com',
+            'password' => '$2y$12$kt6DH3V.hOsZxbIJLNZFEOIaUPB4dSyJL1hepO8GSmE1UgMXvtp3q',
+            'is_admin' => true,
         ]);
-    }
+
+        User::factory()->create([
+            'name' => 'user1',
+            'email' => 'user1@a.com',
+            'password' => '$2y$12$kt6DH3V.hOsZxbIJLNZFEOIaUPB4dSyJL1hepO8GSmE1UgMXvtp3q',
+        ]);
+
+        User::factory()->create([
+            'name' => 'user2',
+            'email' => 'user2@a.com',
+            'password' => '$2y$12$kt6DH3V.hOsZxbIJLNZFEOIaUPB4dSyJL1hepO8GSmE1UgMXvtp3q',
+        ]);
+
+        Employee::factory()->create([
+            'name' => 'employee1',
+            'user_id' => 2,
+        ]);
+
+        Employee::factory()->create([
+            'name' => 'employee2',
+            'user_id' => 2,
+        ]);
+
+        Employee::factory()->create([
+            'name' => 'employee3',
+            'user_id' => 1,
+        ]);
+
+        Employee::factory()->create([
+            'name' => 'employee4',
+            'user_id' => 2,
+        ]);
+    }   
 }
