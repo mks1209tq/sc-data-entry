c5b2c1a66cd4850f4b61ab41af25889eabbb823a
mks1209tq <khalid.siddique@tanseeqinvestment.com>
2024-10-21 12:05:47 +0400

    Scope only for single PO

--- app/Models/Scopes/UserScope.php
+++ app/Models/Scopes/UserScope.php
@@ -22,7 +22,7 @@ class UserScope implements Scope
 
             // For non-admin users
             $builder->where('user_id', Auth::user()->id)
-                    ->where('single_po', 0)
+                    ->where('single_po', 1)
                     ->where('PO_number', '!=', '#N/A')
                     ->where('advance_amount', '!=', 2)
                     ->where('col1', '!=', 2);
