80ef76a1047feec44ca72f7e3fe89880b036df86
Mohammed Maroof <19404@TQ.INV>
2024-10-22 19:49:03 +0400

    new edit page-2

--- app/Models/Scopes/UserScope.php
+++ app/Models/Scopes/UserScope.php
@@ -26,7 +26,7 @@ class UserScope implements Scope
                     // ->where('PO_number', '!=', '#N/A')
                     ->where('col4', '!=', 1)//isUpdated
                     ->where('col7', '!=', 1)//isMulti
-                    ->whereNotLike('col9', '0%');//PO_number_final
+                    ->whereNotLike('col19', '0%');//PO_number_final
 
         } else {
             // For unauthenticated users or as a fallback
