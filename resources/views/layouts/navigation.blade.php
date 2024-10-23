e1fecb013c46e822b4b7cdd5cb252c9bf3085165
Mohammed Maroof <19404@TQ.INV>
2024-10-23 18:04:55 +0400

    mobile navigation fixed 2

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -125,6 +125,10 @@
         <div class="pt-4 pb-1 border-t border-gray-200">
 
             <div class="mt-3 space-y-1">
+                <x-responsive-nav-link :href="route('dashboard')">
+                    {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+                
                 <x-responsive-nav-link :href="route('somethingwrong')">
                     {{ __('Something Wrong') }}
                 </x-responsive-nav-link>
