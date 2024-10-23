cf4a5084b7967bf2b39ae51d42cf3f6387d61fe8
Mohammed Maroof <19404@TQ.INV>
2024-10-23 18:02:56 +0400

    mobile navigation fixed

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -80,12 +80,12 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden border border-gray-200">
+        <!-- <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
-        </div>
+        </div> -->
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
@@ -112,4 +112,35 @@
             </div>
         </div>
     </div>
+
+    <!-- Responsive Navigation Menu -->
+    <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden border border-gray-200">
+        <!-- <div class="pt-2 pb-3 space-y-1">
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+        </div> -->
+
+        <!-- Responsive Settings Options -->
+        <div class="pt-4 pb-1 border-t border-gray-200">
+
+            <div class="mt-3 space-y-1">
+                <x-responsive-nav-link :href="route('somethingwrong')">
+                    {{ __('Something Wrong') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('multi')">
+                    {{ __('Multi PO') }}
+                </x-responsive-nav-link>
+
+                <x-responsive-nav-link :href="route('done')">
+                    {{ __('Done') }}
+
+                </x-responsive-nav-link>
+
+            </div>
+        </div>
+    </div>
+
+    
 </nav>
